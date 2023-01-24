88caf5a9a3ca81fcaf46d491db6a106844c0c54c
Gabriel Klein <gabriel.klein@philips.com>
2023-01-24 18:58:47 -0300

    Added the preflighted requests permission

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,7 @@
 module.exports = (request, response, next) => {
   /* This is a CORS header. It allows the frontend to access the backend. */
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Origin-Methods', '*');
+  response.setHeader('Access-Control-Allow-Origin-Headers', '*');
   next();
 };
