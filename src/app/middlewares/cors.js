62f76f5d63d0e6cbc7f0a83d325e422dadd328a3
Gabriel Klein <gabriel.klein@philips.com>
2023-02-10 06:17:38 -0300

    Adjustment on the cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -2,7 +2,7 @@ module.exports = (request, response, next) => {
   /* This is a CORS header. It allows the frontend to access the backend. */
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   response.setHeader('Access-Control-Allow-Origin-Methods', '*');
-  response.setHeader('Access-Control-Allow-Origin-Headers', '*');
+  response.setHeader('Access-Control-Allow-Headers', 'X-PINGOTHER,Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization');
   /* Setting the maximum age of the CORS header. */
   response.setHeader('Access-Control-Max-Age', '20');
   next();
